cac00bcb0041ad2d6b5fecd97b5eeed2cb7a98b7
Josiel <jdlopez114@gmail.com>
2017-01-08 12:37:47 -0500

    RentACar

--- src/DataStructures.java
+++ src/DataStructures.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class DataStructures {
 
     public static void main(String args[]) {
+
         sortingSentences("Eddie knows what he's doing");
         ArrayList<Integer> array = new ArrayList<>();
         array.add(1);
@@ -15,6 +16,7 @@ public class DataStructures {
         array.add(4);
         System.out.println(clumps(array));
 
+        podHashMap();
     }
 
 //    List<Cats> cats = new ArrayList<>;
@@ -33,20 +35,35 @@ public class DataStructures {
     public static void podHashMap() {
         Map<String, Integer> podMap = new HashMap<>();
         podMap.put("Josiel", 35);
-        podMap.put("Max", 35);
-        podMap.put("Eddie", 35);
-        podMap.put("Que", 35);
-        podMap.put("Ridita", 35);
-        podMap.put("Yojana", 35);
-        podMap.put("Asia", 35);
+        podMap.put("Max", 45);
+        podMap.put("Eddie", 67);
+        podMap.put("Que", 15);
+        podMap.put("Ridita", 25);
+        podMap.put("Yojana", 22);
+        podMap.put("Asia", 55);
 
-        for(String key : podMap.keySet()){
+        for (String key : podMap.keySet()) {
             System.out.println(key + " " + podMap.get(key));
         }
 
 //        for (Map.Entry<String, Integer> entry : podMap.entrySet()) {
 //            System.out.println(entry.getKey() + "/" + entry.getValue());
 //        }
+    }
+
+
+    // 4. canRentACar
+    // Write a method called canRentACar that takes the Pod HashMap as a parameter
+    // and returns whether or not the Pod can rent a car. If one or more studetns is over 25,
+    // it should return true. If no one in the Pod is over 25, it should return false.
+
+    public static boolean canRentACar(HashMap<String, Integer> map) {
+        for (String key : map.keySet()) {
+            if (map.get(key) > 25) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
